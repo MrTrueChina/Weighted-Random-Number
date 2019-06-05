1adf5bb82bde94e7fb9e442dc9b9330897122a4c
Mr.True.China <mr.true.china@gmail.com>
2019-06-05 14:19:05 +0900

    Initial commit

---
+++ Assets/Editor/Tests/DictionaryTest.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using System.Linq;
+
+/// <summary>
+/// 用于测试 Dictionary 的部分方法的效果
+/// </summary>
+[TestFixture]
+public class DictionaryTest
+{
+    Dictionary<int, int> _dictionary;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _dictionary = new Dictionary<int, int>();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _dictionary = null;
+    }
+
+    /// <summary>
+    /// 测试 Dictionary 是不是有序的
+    /// </summary>
+    [Test]
+    public void IsOrderly()
+    {
+        List<int> keys;
+
+        _dictionary.Add(0, 0);
+        _dictionary.Add(1, 1);
+        _dictionary.Add(2, 2);
+        _dictionary.Add(3, 3);
+        _dictionary.Add(4, 4);
+        _dictionary.Add(5, 5);
+
+        keys = _dictionary.Keys.ToList();
+        for(int i = 1; i < keys.Count;i++)
+            if(keys[i]<keys[i-1])
+            {
+                Debug.Log("Dictionary 不是按照存入顺序保持有序的");
+                return;
+            }
+
+        _dictionary.Clear();
+
+        _dictionary.Add(5, 5);
+        _dictionary.Add(4, 4);
+        _dictionary.Add(3, 3);
+        _dictionary.Add(2, 2);
+        _dictionary.Add(1, 1);
+        _dictionary.Add(0, 0);
+
+        keys = _dictionary.Keys.ToList();
+        for (int i = 1; i < keys.Count; i++)
+            if (keys[i] > keys[i - 1])
+            {
+                Debug.Log("Dictionary 不是按照存入顺序保持有序的");
+                return;
+            }
+
+        Debug.Log("Dictionary 是按照存入顺序保持有序的");
+    }
+}
---
+++ Assets/WeightedRandom.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightedRandom
+{
+    Dictionary<int, int> _probabilities;
+
+    public WeightedRandom()
+        : this(new Dictionary<int, int>())
+    { }
+
+    public WeightedRandom(Dictionary<int, int> probabilities)
+    {
+        _probabilities = probabilities;
+    }
+
+    /// <summary>
+    /// 根据存入的加权概率随机返回一个int
+    /// </summary>
+    /// <returns></returns>
+    public int GetInt()
+    {
+        throw new MissingComponentException(); //TODO
+    }
+
+    /// <summary>
+    /// 存入一个值和它的加权概率，如果已经有这个值的概率则会覆盖之前的概率，如果概率小于等于0，则会移除这个值和它的概率
+    /// </summary>
+    /// <param name="value"></param>
+    /// <param name="probability"></param>
+    public void SetProbability(int value,int probability)
+    {
+
+    }
+
+    /// <summary>
+    /// 移除一个值和它的概率
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public bool RemoveProbability(int value)
+    {
+        throw new MissingComponentException(); //TODO
+    }
+
+    public static int GetInt(Dictionary<int, int> probabilities)
+    {
+        return new WeightedRandom(probabilities).GetInt();
+    }
+}
